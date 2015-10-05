934e0250baf39755747ca9dcf22d935de8bc8833
Andrea Jemmett <andreajemmett@gmail.com>
2015-10-05 14:34:23 +0200

    use thymio

--- clean_src/obstacle_avoidance.py
+++ clean_src/obstacle_avoidance.py
@@ -65,18 +65,30 @@ class ObstacleAvoidance(Simulation):
 		# fitness for 1 timestep in [-1000, 1000]
 		return float(motorspeed['left'] + motorspeed['right']) * (1 - speedpenalty) * (1 - sensorpenalty)
 
+
+
+CURRENT_FILE_PATH = os.path.abspath(os.path.dirname(__file__))
+MAIN_LOG_PATH = os.path.join(CURRENT_FILE_PATH, 'log_main')
+
+def getNextIDPath(path):
+	nextID = 0
+	filelist = sorted(os.listdir(path))
+	if filelist and filelist[-1][0].isdigit():
+		nextID = int(filelist[-1][0]) + 1
+	return str(nextID)
+
 if __name__ == '__main__':
 	from peas.methods.neat import NEATPopulation, NEATGenotype
 	genotype = lambda: NEATGenotype(inputs=5, outputs=2)
 	pop = NEATPopulation(genotype, popsize=20)
 
 	# Main logger for ThymioController and CommandsListener
-    mainLogger = logging.getLogger('mainLogger')
-    mainLogger.setLevel(logging.DEBUG)
-    mainLogFilename = getNextIDPath(MAIN_LOG_PATH) + '_' + time.strftime("%Y-%m-%d_%H-%M-%S") + '_main_debug.log'
-    mainHandler = logging.FileHandler(os.path.join(MAIN_LOG_PATH, mainLogFilename))
-    mainHandler.setFormatter(FORMATTER)
-    mainLogger.addHandler(mainHandler)
+	mainLogger = logging.getLogger('mainLogger')
+	mainLogger.setLevel(logging.DEBUG)
+	mainLogFilename = getNextIDPath(MAIN_LOG_PATH) + '_' + time.strftime("%Y-%m-%d_%H-%M-%S") + '_main_debug.log'
+	mainHandler = logging.FileHandler(os.path.join(MAIN_LOG_PATH, mainLogFilename))
+	mainHandler.setFormatter(FORMATTER)
+	mainLogger.addHandler(mainHandler)
 	thymioController = ThymioController(mainLogger)
 	
 	debug = true
