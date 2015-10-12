2095f27354a1e3ac3e789fb78053e8598e44057e
Andrea Jemmett <andreajemmett@gmail.com>
2015-10-12 18:12:30 +0200

    setup experiment and log important stuff

--- clean_src/classes.py
+++ clean_src/classes.py
@@ -34,7 +34,7 @@ TOTAL_WEIGHTS = NN_WEIGHTS + NN_WEIGHTS_HIDDEN  # 40 + 10 = 50
 NN_WEIGHTS_NO_HIDDEN = (NB_DIST_SENS + NB_CAM_SENS + 1) * 2  # (5 + 4 + 1) * 2 = 20
 
 MAXSPEED = 500
-SENSOR_MAX = [3253, 4384, 4733, 4794, 2825]  # 4500  # XXX: found sensor with max value of 5100
+SENSOR_MAX = [4000] * 5  # 4500  # XXX: found sensor with max value of 5100
 TIME_STEP = 0.05  # = 50 milliseconds. IMPORTANT: keep updated with TIME_STEP constant in asebaCommands.aesl
 CAMERA_MAX = [7000, 7000, 9000, 11000, 20000, 20000, 20000, 20000]  # left, right, center, bottom (red and black)
 
--- clean_src/obstacle_avoidance.py
+++ clean_src/obstacle_avoidance.py
@@ -5,42 +5,83 @@ import os
 import time
 import gobject
 import glib
+import dbus
+import dbus.mainloop.glib
+import logging
+import pickle
 import parameters as pr
 import classes
-from Simulation import *
-from ThymioController import *
+from helpers import *
 from peas.networks.rnn import NeuralNetwork
 
-EVALUATIONS = 100
+EVALUATIONS = 1000
+MAX_MOTOR_SPEED = 300
+TIME_STEP = 0.005
+ACTIVATION_FUNC = 'tanh'
+POPSIZE = 1
+GENERATIONS = 100
+SOLVED_AT = EVALUATIONS * 2
+EXPERIMENT_NAME = 'NEAT_obstacle_avoidance'
+
+CURRENT_FILE_PATH = os.path.abspath(os.path.dirname(__file__))
+MAIN_LOG_PATH = os.path.join(CURRENT_FILE_PATH, 'log_main')
+OUTPUT_PATH = os.path.join(CURRENT_FILE_PATH, 'output')
+PICKLED_DIR = os.path.join(CURRENT_FILE_PATH, 'pickled')
+FORMATTER = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+AESL_PATH = os.path.join(CURRENT_FILE_PATH, 'asebaCommands.aesl')
 
 
-class ObstacleAvoidance(Simulation):
-
-	# def __init__(self, thymioController, debug, experiment_name):
-	# 	super(ObstacleAvoidance, self).__init__(thymioController, debug, experiment_name)
-	def __init__(self, thymioController):
+class NEATTask:
+	def __init__(self, thymioController, debug = False, experimentName = 'ObstacleAvoidance'):
 		self.thymioController = thymioController
+		self.logger = logging.getLogger('simulationLogger')
+		logLevel = logging.INFO
+		if debug:
+			logLevel = logging.DEBUG
+		self.logger.setLevel(logLevel)
+		outputDir = os.path.join(OUTPUT_PATH, experimentName)
+		mkdir_p(outputDir)
+		mkdir_p(PICKLED_DIR)
+		logFilename = os.path.join(outputDir, experimentName + '_sim_debug.log')
+		simHandler = logging.FileHandler(logFilename)
+		simHandler.setFormatter(FORMATTER)
+		self.logger.addHandler(simHandler)
+
+	def evaluate(self, evaluee):
+		raise NotImplemented('Evaluate method not implemented')
+
+	def solve(self, evaluee):
+		raise NotImplemented('Evaluate method not implemented')
+
+	def getFitness(self, motorspeed, observation):
+		raise NotImplemented('Evaluate method not implemented')
+
+	def getLogger(self):
+		return self.logger
+
+class ObstacleAvoidance(NEATTask):
 
 	def evaluate(self, evaluee):
 		self.evaluations_taken = 0
 		self.fitness = 0
 		self.loop = gobject.MainLoop()
 		def update_fitness(task, fit):
-			# print 'Doing update_fitness ' + str(task.fitness)
 			task.fitness += fit
 		def main_lambda(task):
 			if task.evaluations_taken == EVALUATIONS:
 				stopThymio(thymioController)
 				task.loop.quit()
 				return False 
-			# print 'Doing __step'
 			ret_value =  self.__step(evaluee, lambda (fit): update_fitness(self, fit))
 			task.evaluations_taken += 1
+			# time.sleep(TIME_STEP)
 			return ret_value
-		handle = gobject.timeout_add(100, lambda: main_lambda(self))  # every 0.1 sec
+		gobject.timeout_add(int(TIME_STEP * 1000), lambda: main_lambda(self))
+		# glib.idle_add(lambda: main_lambda(self))
 		self.loop.run()
 
-		print "Fitness at end: %d" % self.fitness
+		fitness = max(self.fitness, 1)
+		print 'Fitness at end: %d' % fitness
 
 		# self.thymioController.SendEventName('PlayFreq', [700, 0], reply_handler=dbusReply, error_handler=dbusError)
 		# time.sleep(.3)
@@ -48,29 +89,19 @@ class ObstacleAvoidance(Simulation):
 		# time.sleep(0.1)
 		time.sleep(1)
 
-		return { 'fitness': self.fitness }
+		return { 'fitness': fitness }
 
 	def solve(self, evaluee):
-		return int(self.evaluate(evaluee)['fitness'] >= EVALUATIONS)
+		return int(self.evaluate(evaluee)['fitness'] >= SOLVED_AT)
 
 	def __step(self, evaluee, callback):
-		# Read sensors: request to ThymioController
-		# self.thymioController.readSensorsRequest()
-		# self.waitForControllerResponse()
-		# psValues = self.thymioController.getPSValues()
-		# psValues = np.array([psValues[0], psValues[2], psValues[4], psValues[5], psValues[6]])
-		# psValues = np.random.standard_normal(5)
 		def ok_call(psValues):
 			psValues = np.array([psValues[0], psValues[2], psValues[4], psValues[5], psValues[6], 1])
-			# print 'Observation ' + str(psValues)
 
 			left, right = list(NeuralNetwork(evaluee).feed(psValues)[-2:])
 
 			motorspeed = { 'left': left, 'right': right }
 
-			# self.thymioController.writeMotorspeedRequest((left, right))
-			# self.waitForControllerResponse()
-			# print 'Writing to motor ' + str(motorspeed)
 			writeMotorSpeed(self.thymioController, motorspeed)
 
 			callback(self.getFitness(motorspeed, psValues))
@@ -100,41 +131,10 @@ class ObstacleAvoidance(Simulation):
 		if sensorpenalty > 1:
 			sensorpenalty = 1
 
-		# fitness for 1 timestep in [-1000, 1000]
-		return float(motorspeed['left'] + motorspeed['right']) * (1 - speedpenalty) * (1 - sensorpenalty)
-
-class ObstacleAvoidance2(ObstacleAvoidance):
-
-	def getFitness(self, motorspeed, observation):
-		# Calculate penalty for rotating
-		speedpenalty = 0
-		if motorspeed['left'] > motorspeed['right']:
-			speedpenalty = float((motorspeed['left'] - motorspeed['right'])) / float(pr.real_max_speed)
-		else:
-			speedpenalty = float((motorspeed['right'] - motorspeed['left'])) / float(pr.real_max_speed)
-		if speedpenalty > 1:
-			speedpenalty = 1
-
-		# Calculate normalized distance to the nearest object
-		sensorpenalty = 0
-		for i, sensor in enumerate(observation[:-1]):
-			distance = sensor / float(classes.SENSOR_MAX[i])
-			if sensorpenalty < distance:
-				sensorpenalty = distance
-		if sensorpenalty > 1:
-			sensorpenalty = 1
-
-		# Only punish if object is really close
-		if sensorpenalty < .8:
-			sensorpenalty = 0
-
-		# fitness for 1 timestep in [-1000, 1000]
+		# fitness for 1 timestep in [-2, 2]
 		return float(motorspeed['left'] + motorspeed['right']) * (1 - speedpenalty) * (1 - sensorpenalty)
 
 
-CURRENT_FILE_PATH = os.path.abspath(os.path.dirname(__file__))
-MAIN_LOG_PATH = os.path.join(CURRENT_FILE_PATH, 'log_main')
-AESL_PATH = os.path.join(CURRENT_FILE_PATH, 'asebaCommands.aesl')
 
 def getNextIDPath(path):
 	nextID = 0
@@ -144,9 +144,8 @@ def getNextIDPath(path):
 	return str(nextID)
 
 def writeMotorSpeed(controller, motorspeed):
-	speed_constant = 100
-	controller.SetVariable("thymio-II", "motor.left.target", [motorspeed['left'] * speed_constant])
-	controller.SetVariable("thymio-II", "motor.right.target", [motorspeed['right'] * speed_constant])
+	controller.SetVariable("thymio-II", "motor.left.target", [motorspeed['left'] * MAX_MOTOR_SPEED])
+	controller.SetVariable("thymio-II", "motor.right.target", [motorspeed['right'] * MAX_MOTOR_SPEED])
 
 
 def getProxReadings(controller, ok_callback, nok_callback):
@@ -164,31 +163,27 @@ def dbusError(e):
 
 if __name__ == '__main__':
 	from peas.methods.neat import NEATPopulation, NEATGenotype
-	genotype = lambda: NEATGenotype(inputs=6, outputs=2, types=['sigmoid'])
-	pop = NEATPopulation(genotype, popsize=20)
+	genotype = lambda: NEATGenotype(inputs=6, outputs=2, types=[ACTIVATION_FUNC])
+	pop = NEATPopulation(genotype, popsize=POPSIZE)
 
-	# Main logger for ThymioController and CommandsListener
-	# mainLogger = logging.getLogger('mainLogger')
-	# mainLogger.setLevel(logging.DEBUG)
-	# mainLogFilename = getNextIDPath(MAIN_LOG_PATH) + '_' + time.strftime("%Y-%m-%d_%H-%M-%S") + '_main_debug.log'
-	# mainHandler = logging.FileHandler(os.path.join(MAIN_LOG_PATH, mainLogFilename))
-	# mainHandler.setFormatter(FORMATTER)
-	# mainLogger.addHandler(mainHandler)
-
-	# gobject.threads_init()
-	# dbus.mainloop.glib.threads_init()
-
-	# thymioController = ThymioController(mainLogger)
-	# thymioController.run()
 	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 	bus = dbus.SessionBus()
 	thymioController = dbus.Interface(bus.get_object('ch.epfl.mobots.Aseba', '/'), dbus_interface='ch.epfl.mobots.AsebaNetwork')
-	# print thymioController.GetNodesList()
 	thymioController.LoadScripts(AESL_PATH, reply_handler=dbusReply, error_handler=dbusError)
 
-	# debug = True
-	# experiment_name = 'Experiment 001'
-	# task = ObstacleAvoidance(thymioController, debug, experiment_name)
-	task = ObstacleAvoidance2(thymioController)
+	# switch thymio LEDs off
+	thymioController.SendEventName('SetColor', [0, 0, 0, 0], reply_handler=dbusReply, error_handler=dbusError)
+
+	debug = True
+	task = ObstacleAvoidance(thymioController, debug, EXPERIMENT_NAME)
 	
-	pop.epoch(generations=100, evaluator=task, solution=task)
+	def epoch_callback(population):
+		current_champ = population.champions[-1]
+		print 'Champion: ' + str(current_champ.get_network_data())
+
+		task.getLogger().info(', '.join([str(ind.stats['fitness']) for ind in population.population]))
+
+		# current_champ.visualize(os.path.join(CURRENT_FILE_PATH, 'img/obstacle_avoid_%d.jpg' % population.generation))
+		pickle.dump(current_champ, file(os.path.join(PICKLED_DIR, 'obstacle_avoid_%d.txt' % population.generation), 'w'))
+
+	pop.epoch(generations=GENERATIONS, evaluator=task, solution=task, callback=epoch_callback)
