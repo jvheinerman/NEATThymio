9902f649d1ebd042415923992d29618a4d2ff5fe
Chiel Kooijman <chiel999@gmail.com>
2015-12-07 16:42:09 +0100

    Fix

--- clean_src/dist_angle_matrices.py
+++ clean_src/dist_angle_matrices.py
@@ -5,9 +5,9 @@ distances = np.zeros((240, 320))
 angles = np.zeros((240, 320))
 point = 240, 160
 
-for y in range(240):
-    for x in range(320):
-        distances[y, x] = np.sqrt((x - .5 - point[0]) ** 2 +
+for x in range(240):
+    for y in range(320):
+        distances[x, y] = np.sqrt((x - .5 - point[0]) ** 2 +
                                   (y - .5 - point[1]) ** 2)
         angles[x, y] = -(np.arctan2(-(y - .5 - point[1]), -(x - .5 - point[0]))
                          - np.pi / 2)
