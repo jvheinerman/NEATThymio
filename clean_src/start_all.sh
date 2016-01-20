9fceca3aebe2625d8607d06cc7e7810319df7b96
Chiel Kooijman <chiel999@gmail.com>
2016-01-20 15:42:33 +0100

    rsync without sshpass.

--- clean_src/start_all.sh
+++ clean_src/start_all.sh
@@ -3,7 +3,7 @@
 while read line
 do
 	echo "STARTING $line"
-	sshpass -p 'raspberry' rsync --progress -avz -e ssh pi@$line:~/ ./*
+	rsync --progress -avz -e ssh pi:raspberry@$line:~/ ./*
 
 	git_sha="$(git rev-parse --short HEAD)"
 	ssh -X pi@$line './start_one.sh' $line $git_sha &
