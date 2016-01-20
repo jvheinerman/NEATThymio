10f5dd93b7e94b70157d1eb78452cdf044743048
Chiel Kooijman <chiel999@gmail.com>
2016-01-20 15:31:59 +0100

    Use rsync instead of scp; untested but should be faster.

--- clean_src/start_all.sh
+++ clean_src/start_all.sh
@@ -3,7 +3,8 @@
 while read line
 do
 	echo "STARTING $line"
-	scp -r ./* pi@$line:~/
+	sshpass -p 'raspberry' rsync --progress -avz -e ssh pi@$line:~/ ./*
+
 	git_sha="$(git rev-parse --short HEAD)"
 	ssh -X pi@$line './start_one.sh' $line $git_sha &
 done < ./bots.txt
